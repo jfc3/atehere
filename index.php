cea1b15a0b5de1d787c7cf25d451cc0727cd5af1
jfc3 <jacobboomer@msn.com>
2017-04-02 10:26:47 -0400

    Add the ability to have an asterisks to denote places I haven't been and take the out of the name field. Now the questions becomes do i have the asterisk inside the link or after?

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ if (in_array($slct, $locs) && $slct != "") {
 		<p>In the future, I'm planning on creating and application that people can keep track places they want to eat when the travel.</p>
 	<?php } else { ?>
 			<h2><?php echo $city; ?></h2>
-			<p>All the restaurants with an asterisk (<strong>*</strong>) next to them are places I would like to visit that friends, blog posts, articles, TV shows, etc. recommend I eat.</p>
+			<p id="not-eaten">All the restaurants with an asterisk (<strong>*</strong>) next to them are places I would like to visit that friends, blog posts, articles, TV shows, etc. recommend I eat.</p>
 			<p id="ggl-mps"><strong>Note</strong>: All restaurant addresses link to Google maps.</p>
     <?php
 		foreach ($posts as $post) {
@@ -121,9 +121,9 @@ if (in_array($slct, $locs) && $slct != "") {
 	?>
 		<div class="vcard">
 		<?php if ($post->url != "") { ;?>
-			<h3 class="fn" id="<?php echo $rest_name; ?>" tabindex="-1"><a href="<?php echo $post->url; ?>" class="url"><?php echo $post->name; ?></a></h3>
+			<h3 class="fn" id="<?php echo $rest_name; ?>" tabindex="-1"><a href="<?php echo $post->url; ?>" class="url"><?php echo $post->name; ?><?php if ($post->eaten != "yes") { echo '<span aria-describedby="not-eaten"> *</span>';} ?></a></h3>
 			<?php } else { ?>
-			<h3 class="fn" id="<?php echo $rest_name; ?>" tabindex="-1"><?php echo $post->name; ?></h3>
+			<h3 class="fn" id="<?php echo $rest_name; ?>" tabindex="-1"><?php echo $post->name; ?><?php if ($post->eaten != "yes") { echo '<span aria-describedby="not-eaten"> *</span>';} ?></h3>
 	    <?php } ?>
 			<p><a href="https://www.google.com/maps/place/<?php echo urlencode($post->add1.", ".$post->cty.", ".$post->stt.", ".$post->zpcd); ?>" class="rest-addr" aria-describedby="ggl-mps"><span class="adr"><span class="street-address"><?php echo $post->add1; ?></span><br /><?php if ($post->add2 != "") { echo '<span class="extended-address">'.$post->add2."</span><br />";} ?> <span class="locality"><?php echo $post->cty; ?></span>, <span class="region"><?php echo $post->stt; ?></span> <span class="postal-code"><?php echo $post->zpcd; ?></span></span></a></p>
 			<?php if ($post->phn != "") { echo "<p>".$post->phn."</p>";} ?>
