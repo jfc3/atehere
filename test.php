55e1dfe288d6b6730ce23a99254086116614b62b
jfc3 <jacobboomer@msn.com>
2022-10-18 20:07:29 -0400

    Updated code to read the text "Looking forward to eating here." after theme of the restaurant.

--- test.php
+++ test.php
@@ -187,7 +187,7 @@ if (in_array($slct, $locs) && $slct != "") {
 		<p>In the future, I'm planning on creating and application that people can keep track places they want to eat when the travel.</p>
 	<?php } else { ?>
 		<h2><?php echo $city; ?></h2>
-		<p>All the restaurants with an asterisk (<strong>*</strong>) after the name and the text "<strong>Looking forward to eating here.</strong>" at the end of the note are places I would like to visit that friends, blog posts, articles, TV shows, etc. recommend I eat.</p>
+		<p>All the restaurants with an asterisk (<strong>*</strong>) after the name and the text "<span id="lfteh"><strong>Looking forward to eating here.</strong></span>" at the end of the note are places I would like to visit that friends, blog posts, articles, TV shows, etc. recommend I eat.</p>
 		<p id="ggl-mps"><strong>Note</strong>: All restaurant addresses link to Google maps.</p>
 		<ul role="list">
     <?php
@@ -197,7 +197,8 @@ if (in_array($slct, $locs) && $slct != "") {
 			<li>
 					<div class="vcard">
 					<?php if ($post->url != "" && $post->url != "hhh") { ;?>
-						<h3 class="fn" id="<?php echo $rest_name; ?>" tabindex="-1"><a href="<?php echo $post->url; ?>" class="url"><?php echo $post->name; ?><?php if ($post->eaten != "yes" && $post->eaten != "jjj") { echo ' * <span class="scrn-rdr">Looking forward to eating here.</span>';} ?></a></h3>
+						<h3 class="fn" id="<?php echo $rest_name; ?>" tabindex="-1">
+							<a href="<?php echo $post->url; ?>" class="url"><?php echo $post->name; ?><?php if ($post->eaten != "yes" && $post->eaten != "jjj") { echo ' <span aria-labelledby="lfteh">*</span>';} ?></a></h3>
 					<?php } else { ?>
 						<h3 class="fn" id="<?php echo $rest_name; ?>" tabindex="-1"><?php echo $post->name; ?><?php if ($post->eaten != "yes" && $post->eaten != "jjj") { echo ' * <span class="scrn-rdr">Looking forward to eating here.</span>';} ?></h3>
 				    <?php } ?>
